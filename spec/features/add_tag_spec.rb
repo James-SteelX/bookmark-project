c370058ccdf00ea2632b2d2bbaf09af5cc887b94
James <James.Steel@me.com>
2016-10-19 09:57:42 +0100

    all green after ch17

--- spec/features/add_tag_spec.rb
+++ spec/features/add_tag_spec.rb
@@ -7,9 +7,21 @@ feature 'adding a tag' do
     visit '/links/new'
     fill_in :title, with: 'New link'
     fill_in :url, with: 'www.bbc.co.uk'
-    fill_in :tags, with: 'current affairs'
+    fill_in :tags, with: 'news'
     click_button 'Save'
     link = Link.first
-    expect(link.tags.map(&:name)).to include ('current affairs')
+    expect(link.tags.map(&:name)).to include ('news')
   end
 end
+
+ feature 'adding multiple tags' do
+   scenario 'add more than one tage to a link' do
+     visit '/links/new'
+     fill_in :title, with: 'New link'
+     fill_in :url, with: 'www.bbc.co.uk'
+     fill_in :tags, with: 'current affairs'
+     click_button 'Save'
+     link = Link.first
+     expect(link.tags.map(&:name)).to include('current', 'affairs')
+   end
+ end
