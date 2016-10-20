df4eb3f3dbb6e3f054fc99c0f6598f629dff243c
Tom Nunn <t@tomnunn.uk>
2016-10-20 11:23:17 +0100

    prevent duplicate registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,10 @@ class User
 	attr_accessor :password_confirmation
 
 	property :id, Serial
-	property :email, String, :required => true
+	property :email, String, format: :email_address, required: true, unique: true
 	property :password_digest, Text
 
 	validates_confirmation_of :password
-	validates_presence_of :email
-	validates_format_of :email, :as => :email_address
 
 	def password=(password)
 		@password = password
--- spec/features/creating_user_spec.rb
+++ spec/features/creating_user_spec.rb
@@ -1,15 +1,12 @@
 feature 'Creating users' do
 	scenario 'creating a user' do
-		create_user('ben@example.com', 'password', 'password')
-		initial_length = User.all.length
-		create_user('ben@example.com', 'password', 'password')
-		expect(User.all.length).to be > initial_length
+		expect { create_user('ben@example.com', 'password', 'password') }.to change(User, :count).by(1)
 	end
 
 	scenario 'user provides mismatching passwords' do
 		expect { create_user('ben@example.com', 'apples', 'pears') }.not_to change(User, :count)
 	  expect(current_path).to eq('/users')
-	  expect(page).to have_content 'Password and confirmation password do not match'
+	  expect(page).to have_content 'Password does not match the confirmation'
 	end
 
 	scenario 'user can\'t submit blank email' do
@@ -20,6 +17,12 @@ feature 'Creating users' do
 	scenario 'user can\'t submit wrongly formatted email' do
 		expect { create_user('bjds`bvbvnjvks.bvk', 'pears', 'pears') }.not_to change(User, :count)
 		expect(current_path).to eq('/users')
+	end
+
+	scenario 'user can not sign up with registered email' do
+		create_user('ben@example.com', 'pears', 'pears')
+		expect { create_user('ben@example.com', 'pears', 'pears') }.not_to change(User, :count)
+		expect(page).to have_content 'Email is already taken'
 	end
 
 end
